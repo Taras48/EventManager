da514ac5efcc134de7768a9d67a3e41f82ad9c08
TKukharyuk <TKukharyuk@sportmasterlab.net>
2026-07-29 21:58:53 +0300

    feature/sprint1-lesson2 @PostConstruct, AOP, удаление

---
+++ src/main/java/com/tk/eventmanager/aspect/LoggingAspect.java
@@ -0,0 +1,34 @@
+package com.tk.eventmanager.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Around("execution(* com.eventmanager.service..*(..))")
+    //        │              │                    │
+    //        │              │                    └─ любые аргументы
+    //        │              └─ любой метод в пакете service и подпакетах
+    //        └─ перехватываем "вокруг" (до и после)
+    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        String method = joinPoint.getSignature().toShortString();
+        Object[] args = joinPoint.getArgs();
+
+        System.out.println("▶ " + method + " | args: " + Arrays.toString(args));
+        long start = System.currentTimeMillis();
+
+        Object result = joinPoint.proceed();  // ← вызываем оригинальный метод
+
+        long ms = System.currentTimeMillis() - start;
+        System.out.println("◀ " + method + " | " + ms + " ms | result: " + result);
+
+        return result;
+    }
+}
